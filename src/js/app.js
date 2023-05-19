11e15423f060e18acff92b46dba293cf062ecb67
Carlos Olivares Croc <54370581+olivarescroc@users.noreply.github.com>
2023-05-19 22:15:54 +0000

    para entrega de proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,13 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Nombre" : variables.name} 
+          ${variables.lastname == null ? "Apellido" : variables.lastname}</h1>
+          <h2>${variables.role == null ? "Rol" : variables.role}</h2>
+          <h3>${variables.city == null ? "Ciudad" : variables.city}, ${
+    variables.country == null ? "País" : variables.country
+  }</h3>
+           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -56,7 +59,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/men/41.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
